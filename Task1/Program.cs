af60aec635994bf84ee8490322abd1746827aba2
Mateusz Wyczawski <wyczawski93@gmail.com>
2018-12-18 15:30:59 +0100

    Init commit

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        static void FizzBuzz(UInt16 number)
+        {
+
+        }
+
+        static void DeepDive(UInt16 nestedLevel, string directoryName = "")
+        {
+
+        }
+
+        static void DrownItDown(UInt16 number)
+        {
+
+        }
+
+        static void Exit()
+        {
+
+        }
+    }
+}
