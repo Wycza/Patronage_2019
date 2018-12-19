532eda49328e457ba2742583a5bb0c7244bb3d38
Mateusz Wyczawski <wyczawski93@gmail.com>
2018-12-19 16:31:56 +0100

    Moved DeepDive method to DeepDiveClass.

---
+++ Task_1/Task_1/DeepDiveClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Task_1
+{
+    public static class DeepDiveClass
+    {
+        public static void PrintInstruction()
+        {
+            Console.WriteLine("Create nested directories providing level of nesting.");
+        }
+
+        /// <summary>
+        /// Method which creates nested directories using recurrence.
+        /// </summary>
+        /// <param name="nestingLevel">Level of nesting</param>
+        /// <param name="directoryName">Name of the directory created at current level of nesting</param>
+        public static void DeepDive(UInt16 nestingLevel, string directoryName = "")
+        {
+            if ((nestingLevel < 0) || (nestingLevel > 5))
+            {
+                throw new Exception("Number must be between 0 - 5");
+            }
+
+            try
+            {
+                if (String.IsNullOrEmpty(directoryName))
+                {
+                    directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                }
+
+                if (nestingLevel > 0)
+                {
+                    --nestingLevel;
+                    string folderName = Path.Combine(directoryName, Guid.NewGuid().ToString());
+                    Directory.CreateDirectory(folderName);
+                    Console.WriteLine($"Created folder: {folderName}");
+
+                    DeepDive(nestingLevel, folderName);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return;
+        }
+    }
+}
