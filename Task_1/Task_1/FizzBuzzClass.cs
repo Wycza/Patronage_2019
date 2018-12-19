68c94c0b7c8623643c3cd0e208f8d0d850608aec
Mateusz Wyczawski <wyczawski93@gmail.com>
2018-12-19 15:03:57 +0100

    Moved FizzBuzz to separate class.

---
+++ Task_1/Task_1/FizzBuzzClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_1
+{
+    public static class FizzBuzzClass
+    {
+        public static void PrintInstruction()
+        {
+            Console.WriteLine("For multiples of three print \"Fizz\" instead of the number " +
+                            "and for the multiples of five print \"Buzz\". " +
+                            "For numbers which are multiples of both three and five print \"FizzBuzz\".");
+        }
+
+        /// <summary>
+        /// For multiples of three print "Fizz" instead of the number
+        /// and for the multiples of five print "Buzz".
+        /// For numbers which are multiples of both three and five print "FizzBuzz".
+        /// </summary>
+        /// <param name="number">Number to check</param>
+        public static void FizzBuzz(UInt16 number)
+        {
+            if ((number < 0) || (number > 1000))
+            {
+                throw new Exception("Number must be between 0 - 1000");
+            }
+
+            if ((number % 2 == 0) && (number % 3 == 0))
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Buzz");
+            } else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
