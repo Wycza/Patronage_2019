e4fa64dbe5e3af3c3d1ff50fcc9407208301b815
Mateusz Wyczawski <wyczawski93@gmail.com>
2018-12-22 01:12:09 +0100

    Added comments to FizzBuzz controller.

--- Task_1_Extra/Task_1_Extra/Task_1_Extra/Controllers/FizzBuzzController.cs
+++ Task_1_Extra/Task_1_Extra/Task_1_Extra/Controllers/FizzBuzzController.cs
@@ -4,15 +4,23 @@ using Task_1_Extra.Application.FizzBuzzs.Queries;
 
 namespace Task_1_Extra.Controllers
 {
-    [Route("api/[controller]")]
+    /// <summary>
+    /// This is a FizzBuzz controller.
+    /// </summary>
     [ApiController]
     public class FizzBuzzController : BaseController
     {
-        // GET api/fizz/5
+        /// <summary>
+        /// For multiples of three return "Fizz" instead of the number
+        /// and for the multiples of five return "Buzz".
+        /// For numbers which are multiples of both three and five return "FizzBuzz".
+        /// </summary>
+        /// <param name="number">Number to check</param>
+        /// <returns>Fizz, Buzz, FizzBuzz or {number}</returns>
         [HttpGet("{number}")]
+        [Produces("text/plain")]
         public async Task<ActionResult<string>> Get(int number)
         {
-            //return Ok(new FizzBuzzQuery { Number = number });
             return Ok(await Mediator.Send(new FizzBuzzQuery { Number = number }));
         }
     }
