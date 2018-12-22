630d78d59f41440705354f6e0f40a5ea2e8ab786
Mateusz Wyczawski <wyczawski93@gmail.com>
2018-12-22 01:00:52 +0100

    Implemented FizzBuzz query handler.

--- Task_1_Extra/Task_1_Extra/Task_1_Extra.Application/FizzBuzzs/Queries/FizzBuzzQueryHandler.cs
+++ Task_1_Extra/Task_1_Extra/Task_1_Extra.Application/FizzBuzzs/Queries/FizzBuzzQueryHandler.cs
@@ -9,7 +9,29 @@ namespace Task_1_Extra.Application.FizzBuzzs.Queries
     {
         public async Task<string> Handle(FizzBuzzQuery request, CancellationToken cancellationToken)
         {
-            return $"Not yet implemented. Value = {request.Number}";
+            int number = request.Number;
+
+            if ((number < 0) || (number > 1000))
+            {
+                throw new Exception("Number must be between 0 - 1000");
+            }
+
+            if ((number % 2 == 0) && (number % 3 == 0))
+            {
+                return "FizzBuzz";
+            }
+            else if (number % 2 == 0)
+            {
+                return  "Fizz";
+            }
+            else if (number % 3 == 0)
+            {
+                return  "Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
         }
     }
 }
